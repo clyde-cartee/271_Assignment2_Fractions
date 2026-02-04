c64a0d3bc5905936ede1e3b5f307fe8fb7938a3a
unknown <clydemail.inc@gmail.com>
2026-02-04 18:38:57 -0500

    Set up first file needs and starting fraction class

---
+++ CSCI271_Assignment2_ClydeLeeFisherCartee_W30648739.java
@@ -0,0 +1,48 @@
+/*****************************************************************************************
+* - Project: Assignment 2
+*
+* - Author: Clyde Cartee
+*
+* - Compiler: javac 25.0.1
+*
+* - Date 1/23/25
+*
+* - Purpose: :This is a program to work with fractions without allowing the use of doubles. 
+*
+* - I used W3schools java documentation to help me with syntax
+*************************************************************************************************/
+
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* <Clyde Cartee>
+* <W30648739>
+********************************************************************/
+
+public class CSCI271_Assignment2_ClydeLeeFisherCartee_W30648739 {
+
+    static void Fraction(int... fractionInts){
+
+
+
+    }
+
+
+    public static void main(String args){
+
+
+    }
+
+    
+}
